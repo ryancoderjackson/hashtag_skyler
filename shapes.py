c4e77085986984bc528f425604fe7ed24cb26302
ryancalebjackson <rc70jackson@gmail.com>
2024-10-25 00:49:06 -0500

    shapes

---
+++ shapes.py
@@ -0,0 +1,20 @@
+# The area of a rectangle
+a = int(input("Enter the length of the rectangle "))
+b = int(input("Enter the width of the rectangle "))
+
+print(f"The area of the rectangle with the lenght being {a} and the width being {b} is: {a * b}")
+
+print("")
+# The area of a circle
+c = int(input("Enter the radius of the circle "))
+import math
+d = math.pi
+
+print(f"The area of the circle with a radius of {c} is: {d * c**2}")
+
+print("")
+# The area of a triangle
+e = int(input("Enter the base of the triangle "))
+f = int(input("Enter the height of the triangle "))
+
+print(f"The area of the triangle with the base being {e} and the height being {f} is: {f * e / 2}")
